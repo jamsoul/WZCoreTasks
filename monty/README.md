0e463a157ee90313ca716bb48797a08428823ea4
Jorge Marino <jamsoul@gmail.com>
2022-10-29 17:38:06 -0300

    Final

---
+++ monty/README.md
@@ -0,0 +1,12 @@
+# Monty Hall Problem Simulation
+
+## Building
+
+mkdir build
+cd build
+cmake ..
+make
+
+## Execute
+
+Usage : ./monty [number of iterations]  . Example ./monty 1500 (Runs simulation with 1500 iterations)
--- monty/main.cpp
+++ monty/main.cpp
@@ -9,28 +9,31 @@ void printUsage( char * program ) {
 }
 
 int main(int argc, char** argv) {
-    int iterations = 0;
-    int countWinStay = 0;
+    // Ugly Var Declaration
+    int iterations     = 0;
+    int countWinStay   = 0;
     int countWinSwitch = 0;
-    int carDoor = 0;
-    int pickedDoor = 0;
-    auto commandName = argv[0];
+    int carDoor        = 0;
+    int pickedDoor     = 0;
+    auto commandName   = argv[0];
 
-    if (argc == 1) {
-        printUsage(commandName);
-        return -1;
-    } else if (argc == 2) {
-        iterations = atoi(argv[1]);
-        cout << "Running Monty Hall Problem Simulation with " << iterations << " iterations : " << endl;
+
+    // Check arguments
+    if (argc == 2) {
+        iterations = strtol(argv[1], (char**)NULL, 10);
+        if (iterations > 0) {
+            cout << "Running Monty Hall Problem Simulation with " << iterations << " iterations : " << endl;
+        }
     } else {
-        cout << "Too many Arguments. " << endl;
         printUsage(commandName);
         return -1;
     }
 
-    for (int k=0; k<iterations; k++) {
-        carDoor = rand()%MontyHallDoors;
-        pickedDoor = rand()%MontyHallDoors;
+
+    // Run sim
+    for (int k = 0; k < iterations; k++) {
+        carDoor    = rand() % MontyHallDoors;
+        pickedDoor = rand() % MontyHallDoors;
 
         if (carDoor == pickedDoor) {
             countWinStay++;
@@ -39,8 +42,11 @@ int main(int argc, char** argv) {
         }
     }
 
+
+    // Output results
     std::cout << "Stay Chance   " << 100.0 * (float)countWinStay   / (float)iterations << "%" << std::endl;
     std::cout << "Switch Chance " << 100.0 * (float)countWinSwitch / (float)iterations << "%" << std::endl;
+
     return 0;
 }
 
